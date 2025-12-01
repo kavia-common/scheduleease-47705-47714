197178310465747d0e7f671dcdc85e37da811fec
Kavia AI <production@kavia.ai>
2025-12-01 04:41:12 +0000

    Checkpoint 1 - cg47cdb01d

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/scheduleease-47705-47714/calendar_frontend
+npx eslint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+ if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
